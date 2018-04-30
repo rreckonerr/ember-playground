489d82053d8ab6edd9cac1eafbcdf2b0efd80d89
rreckonerr <rreckonerr@gmail.com>
2018-04-30 14:44:54 +0700

    added all tests except of people-list, programmers and scientists

--- tests/acceptance/ember-quickstart-test.js
+++ tests/acceptance/ember-quickstart-test.js
@@ -5,9 +5,23 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | ember quickstart', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /', async function(assert) {
-    await visit('/');
-
-    assert.equal(currentURL(), '/');
+  test('should show rentals as the home page', async function (assert) {
   });
+
+  test('should link to information about the company', async function (assert) {
+  });
+
+  test('should link to contact information.', async function (assert) {
+  });
+
+  test('should list available rentals.', async function (assert) {
+  });
+
+  test('should filter the list of rentals by city.', async function (assert) {
+  });
+
+  test('should show details for a selected rental', async function (assert) {
+  });
+
+
 });
