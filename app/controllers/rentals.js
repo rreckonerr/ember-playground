b7c8dd970a86ac49722b97a262f8867b6c396f61
rreckonerr <rreckonerr@gmail.com>
2018-05-15 18:24:21 +0700

    defined new rentals controller

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -1,4 +1,13 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+    filterByCity(param) {
+      if (param !== '') {
+        return this.get('store').query('rental', { city: param });
+      } else {
+        return this.get('store').findAll('rental');
+      }
+    }
+  }
 });
