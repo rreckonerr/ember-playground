3ee2438e1ebb616fb5502eb14c71eb26f69e1376
rreckonerr <rreckonerr@gmail.com>
2018-05-15 12:31:11 +0700

    generated rental-listing component

--- tests/acceptance/ember-quickstart-test.js
+++ tests/acceptance/ember-quickstart-test.js
@@ -17,20 +17,20 @@ module('Acceptance | ember quickstart', function(hooks) {
   });
 
   test('should show rentals as the home page', async function (assert) {
-  	await visit('/');
-  	assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
 
   test('should link to information about the company', async function(assert) {
-  	await visit('/');
-  	await click(".menu-about");
-  	assert.equal(currentURL(), '/about', 'should navigate to about');
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to about');
   });
 
   test('should link to contact information', async function(assert) {
-  	await visit('/');
-  	await click(".menu-contact");
-  	assert.equal(currentURL(), '/contact', 'should navigate to contact');
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });  
 
 
