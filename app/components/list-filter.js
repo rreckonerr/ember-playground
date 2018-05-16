dc48953aebf51189f8daded062a37f01088a2d7b
rreckonerr <rreckonerr@gmail.com>
2018-05-16 11:02:19 +0700

    installed yarn and made some changes to contr and comp

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -6,15 +6,20 @@ export default Component.extend({
 
   init() {
     this._super(...arguments);
-    this.get('filter')('').then((results) => this.set('results', results));
+    this.get('filter')('').then((allResults) => {
+      this.set('results', allResults.results);
+    });
   },
 
   actions: {
     handleFilterEntry() {
       let filterInputValue = this.get('value');
       let filterAction = this.get('filter');
-      filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults));
+      filterAction(filterInputValue).then((filterResults) => {
+        if (filterResults.query === this.get('value')) {
+          this.set('results', filterResults.results);
+        }
+      });
     }
   }
-
 });
